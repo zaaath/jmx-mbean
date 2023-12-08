12bda889c94c88339b5f04344252b344179d57df
Leo Toff <leo@toff.dev>
2023-12-07 17:46:14 -0800

    Expose an MBeans probe called message

--- src/main/java/com/mbean/app/HelloWorld.java
+++ src/main/java/com/mbean/app/HelloWorld.java
@@ -1,7 +1,38 @@
 package com.mbean.app;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class HelloWorld implements HelloWorldMBean {
+  private String message = "Initial Message";
+  private int counter = 0;
+  private Timer timer;
+
+  public HelloWorld() {
+    startCounterUpdateTask();
+  }
+
+  private void startCounterUpdateTask() {
+    timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        System.out.println("Counter: " + counter);
+        setMessage("Counter: " + counter);
+        counter++;
+      }
+    }, 0, 500);
+  }
+
   public void sayHello() {
     System.out.println("Hello, world (HelloWorldMBean)!");
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
   }
 }
--- src/main/java/com/mbean/app/HelloWorldMBean.java
+++ src/main/java/com/mbean/app/HelloWorldMBean.java
@@ -2,4 +2,6 @@ package com.mbean.app;
 
 public interface HelloWorldMBean {
   void sayHello();
+  String getMessage();
+  void setMessage(String message);
 }
